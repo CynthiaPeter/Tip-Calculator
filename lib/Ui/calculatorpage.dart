7a95b58558bf58f3d6066f71364f3274320e33da
Cynthia Peter <cynthiapeterpearl@gmail.com>
2021-02-13 22:17:45 +0100

    project 90% done

--- lib/Ui/calculatorpage.dart
+++ lib/Ui/calculatorpage.dart
@@ -6,6 +6,11 @@ class CalculatorUi extends StatefulWidget {
 }
 
 class _CalculatorUiState extends State<CalculatorUi> {
+  double _editableTipPercentage = 0.0;
+  double _editableBill = 0.0;
+  double _totalBill = 0.0;
+  double totalTip = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,73 +25,100 @@ class _CalculatorUiState extends State<CalculatorUi> {
               elevation: 20,
               borderRadius: BorderRadius.circular(10),
               child: Container(
-                  width: MediaQuery.of(context).size.width * 0.8,
-                  height: MediaQuery.of(context).size.height * 0.4,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.transparent,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Color(0XFF22222E),
-                        blurRadius: 0.2,
-                      )
-                    ],
-                    border: Border.all(
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: MediaQuery.of(context).size.height * 0.4,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.transparent,
+                  boxShadow: [
+                    BoxShadow(
                       color: Color(0XFF22222E),
-                    ),
+                      blurRadius: 0.2,
+                    )
+                  ],
+                  border: Border.all(
+                    color: Color(0XFF22222E),
                   ),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(39),
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(39),
+                      child: Column(
+                        children: [
+                          Text(
+                            'Bill Amount',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Color(0xff484856),
+                            ),
+                          ),
+                          SizedBox(height: 10.5),
+                          Container(
+                            width: MediaQuery.of(context).size.width * 0.4,
+                            child: TextField(
+                              keyboardType: TextInputType.numberWithOptions(
+                                  decimal: true),
+                              style: TextStyle(
+                                fontSize: 36,
+                                color: Color.fromRGBO(255, 255, 255, 0.55),
+                              ),
+                              textAlign: TextAlign.center,
+                              decoration: InputDecoration(
+                                // prefixIcon: Icon(Icons.attach_money),
+                                // enabledBorder: OutlineInputBorder(borderSide: BorderSide( color: Colors.white),),
+                                hintText: ('\$0.0'),
+                                hintStyle: TextStyle(
+                                  fontSize: 36,
+                                  color: Color.fromRGBO(255, 255, 255, 0.55),
+                                ),
+                              ),
+                              onChanged: (String value) {
+                                try {
+                                  _editableBill = double.parse(value);
+                                } catch (exception) {
+                                  _editableBill = 0.0;
+                                }
+                              },
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Padding(
+                        padding: EdgeInsets.all(20),
                         child: Column(
                           children: [
                             Text(
-                              'Bill Amount',
+                              // 'Tip\%',
+                              '$_editableTipPercentage%',
                               style: TextStyle(
                                 fontSize: 18,
                                 color: Color(0xff484856),
                               ),
                             ),
-                            SizedBox(height: 10.5),
-                            Container(
-                              width: MediaQuery.of(context).size.width * 0.4,
-                              child: TextField(
-                                keyboardType: TextInputType.numberWithOptions(
-                                    decimal: true),
-                                style: TextStyle(
-                                  fontSize: 36,
-                                  color: Color.fromRGBO(255, 255, 255, 0.55),
-                                ),
-                                textAlign: TextAlign.center,
-                                decoration: InputDecoration(
-                                 // prefixIcon: Icon(Icons.attach_money),
-                                  hintText: ('\$500'),
-                                  hintStyle: TextStyle(
-                                    fontSize: 36,
-                                    color: Color.fromRGBO(255, 255, 255, 0.55),
-                                  ),
-                                ),
-                              ),
+                            Slider(
+                              min: 0,
+                              max: 50,
+                              activeColor: Colors.white,
+                              inactiveColor: Color(0xff484856),
+                              divisions: 10,
+                              value: _editableTipPercentage,
+                              onChanged: (double tipValue) {
+                                setState(() {
+                                  _editableTipPercentage = tipValue;
+
+                                  calculateTotalTip(
+                                      _editableBill, _editableTipPercentage);
+                                  calculateTotalBill(_editableBill, totalTip);
+                                });
+                              },
                             )
                           ],
-                        ),
-                      ),
-                      Padding(
-                          padding: EdgeInsets.all(20),
-                          child: Column(
-                            children: [
-                              Text(
-                                'Tip\%',
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  color: Color(0xff484856),
-                                ),
-                              ),
-                            ],
-                          ))
-                    ],
-                  )),
+                        ))
+                  ],
+                ),
+              ),
             ),
             SizedBox(height: 59),
             Material(
@@ -123,7 +155,7 @@ class _CalculatorUiState extends State<CalculatorUi> {
                           ),
                           SizedBox(height: 12),
                           Text(
-                            '\$575',
+                            '\$$_totalBill',
                             style: TextStyle(
                                 color: Color(0xffDADADA), fontSize: 48),
                           ),
@@ -145,7 +177,7 @@ class _CalculatorUiState extends State<CalculatorUi> {
                                     height: 12,
                                   ),
                                   Text(
-                                    '\$75',
+                                    '\$ $totalTip',
                                     style: TextStyle(
                                       fontSize: 14,
                                       color: Color(0xffDADADA),
@@ -179,4 +211,21 @@ class _CalculatorUiState extends State<CalculatorUi> {
       ),
     );
   }
+// CAalculate total
+
+  calculateTotalTip(
+    double totalBill,
+    double editableTipPercentage,
+  ) {
+    setState(() {
+      totalTip = (totalBill * editableTipPercentage) / 100;
+    });
+  }
+
+  calculateTotalBill(double editableBill, double totalTip) {
+    setState(() {
+      _totalBill = _editableBill + totalTip;
+    });
+  }
+
 }
